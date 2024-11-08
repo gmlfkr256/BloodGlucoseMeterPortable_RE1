12e5b65ad72d5ce0c340ce9c0a1e8c5e02f0bb66
hrkim <rlagmlfkr@naver.com>
2024-11-08 16:10:04 +0900

    자동 커밋: 2024-11-08 16:10:04

--- Page/pagepassword.cpp
+++ Page/pagepassword.cpp
@@ -19,8 +19,9 @@ void PagePassword::init()
     {
         labelButtonNum[i] = new QLabel(this);
         labelButtonNum[i]->setStyleSheet("border: 1px solid #ebebeb;");
-        labelButtonNum[i]->setText(QString::number(i));
+        labelButtonNum[i]->setFont(textResource.getFont(PAGE_PASSWORD,"labeButtonNum"));
         labelButtonNum[i]->setAlignment(Qt::AlignCenter);
+        labelButtonNum[i]->setText(QString::number(i));
 
         if(i<5)
         {
--- textresource.cpp
+++ textresource.cpp
@@ -14,12 +14,19 @@ TextResource::TextResource()
 void TextResource::init()
 {
     //KR
+    //PAGE_PASSWORD
+    fontData[KR][PAGE_PASSWORD].insert("labelButtonNum",QFont(instance.fontSuit,instance.pixelToPoint(44),QFont::Bold));
+
     //PAGE_HOME
     fontData[KR][PAGE_HOME].insert("labelButtonTitle",QFont(instance.fontSuit,instance.pixelToPoint(33),QFont::Bold));
     textData[KR][PAGE_HOME].insert("labelButtonTitle",QStringList{"기상","조식 전","조식 후","중식 전","중식 후","석식 전","석식 후","취침"});
 
     fontData[KR][PAGE_HOME].insert("labelButtonText",QFont(instance.fontSuit,instance.pixelToPoint(62),QFont::ExtraBold));
+
     //EN
+    //PAGE_PASSWORD
+    fontData[EN][PAGE_PASSWORD].insert("labelButtonNum",QFont(instance.fontSuit,instance.pixelToPoint(44),QFont::Bold));
+
     //PAGE_HOME
     fontData[EN][PAGE_HOME].insert("labelButtonTitle",QFont(instance.fontSuit,instance.pixelToPoint(26),QFont::Bold));
     textData[EN][PAGE_HOME].insert("labelButtonTitle",QStringList{"Wake\nUp","Before\nBreakfast","After\nBreakfast","Before\nLunch","After\nLunch","Before\nDinner","After\nDinner","Bed\nTime"});
