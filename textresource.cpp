717554bce23e93f21f7e1403c5d57e77101ec1fa
hrkim <rlagmlfkr@naver.com>
2024-11-08 16:32:55 +0900

    자동 커밋: 2024-11-08 16:32:55

--- textresource.cpp
+++ textresource.cpp
@@ -19,7 +19,7 @@ void TextResource::init()
     textData[KR][PAGE_PASSWORD].insert("labelTitle",QStringList{"로그인을 하세요"});
     fontData[KR][PAGE_PASSWORD].insert("labelTitle",QFont(instance.fontSuit,instance.pixelToPoint(40),QFont::Bold));
     textData[KR][PAGE_PASSWORD].insert("labelTitleSub",QStringList{"비밀번호 4자리를 눌러주세요"});
-    fontData[KR][PAGE_PASSWORD].insert("labelTitleSub",QFont(instance.fontSuit,instance.pixelToPoint(30),QFont::Bold));
+    fontData[KR][PAGE_PASSWORD].insert("labelTitleSub",QFont(instance.fontSuit,instance.pixelToPoint(30)));
 
     //PAGE_HOME
     fontData[KR][PAGE_HOME].insert("labelButtonTitle",QFont(instance.fontSuit,instance.pixelToPoint(33),QFont::Bold));
@@ -33,7 +33,7 @@ void TextResource::init()
     textData[EN][PAGE_PASSWORD].insert("labelTitle",QStringList{"Please log in"});
     fontData[EN][PAGE_PASSWORD].insert("labelTitle",QFont(instance.fontSuit,instance.pixelToPoint(40),QFont::Bold));
     textData[EN][PAGE_PASSWORD].insert("labelTitleSub",QStringList{"Please enter your 4-digit password"});
-    fontData[EN][PAGE_PASSWORD].insert("labelTitleSub",QFont(instance.fontSuit,instance.pixelToPoint(30),QFont::Bold));
+    fontData[EN][PAGE_PASSWORD].insert("labelTitleSub",QFont(instance.fontSuit,instance.pixelToPoint(30)));
 
     //PAGE_HOME
     fontData[EN][PAGE_HOME].insert("labelButtonTitle",QFont(instance.fontSuit,instance.pixelToPoint(26),QFont::Bold));
