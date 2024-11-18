32ea81368ddd660c2d206fa6980c6a7d41db5aa7
hrkim <rlagmlfkr@naver.com>
2024-11-18 14:51:12 +0900

    자동 커밋: 2024-11-18 14:51:12

--- Page/pagegarph.cpp
+++ Page/pagegarph.cpp
@@ -79,7 +79,7 @@ void PageGarph::update()
         "    background-position: center;"
         "}"
     );
-    labelProgressBar->setFixedWidth(100);
+    labelProgressBar->setFixedWidth(40);
 
     labelAdcText->setFont(textResource.getFont(PAGE_GRAPH,"labelAdcText"));
     labelAdcText->setText(textResource.getText(PAGE_GRAPH,"labelAdcText").at(0)+"9999");
