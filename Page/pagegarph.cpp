a46ec29873f31a128a66f83abc5838e3978fe036
hrkim <rlagmlfkr@naver.com>
2024-11-18 17:43:27 +0900

    자동 커밋: 2024-11-18 17:43:27

--- Page/pagegarph.cpp
+++ Page/pagegarph.cpp
@@ -141,7 +141,7 @@ void PageGarph::updatePainter()
 
     if(nProgressValue <= 100)
     {
-        qDebug()<<"nPorgrressValue: "<<nProgressValue;
+        qDebug()<<"nPorgressValue: "<<nProgressValue;
         labelProgressValue->setText(QString::number(nProgressValue));
         labelProgressBar->setGeometry(20,100,nProgressValue*6,20);
     }
