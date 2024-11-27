be0a4158e32b3b28d24043754cd6220b0b9f95b5
hrkim <rlagmlfkr@naver.com>
2024-11-27 10:42:34 +0900

    자동 커밋: 2024-11-27 10:42:34

--- Component/componentspinner.cpp
+++ Component/componentspinner.cpp
@@ -49,9 +49,7 @@ void ComponentSpinner::update()
     instance.pixLoad(labelButtonBottomArrow,false,strDirPath,"/imgArrowBottom.png");
 
     qDebug()<<"labelButtonTop Width; "<<labelButtonTop->width()<<", height: "<<labelButtonTop->height();
-    //labelButtonTopArrow->setGeometry((labelButtonTop->width()-30)/2,(labelButtonTop->height()-14)/2,30,14);
-    labelButtonTopArrow->setGeometry(0,(labelButtonTop->height()-14)/2,30,14);
-    labelButtonBottomArrow->setGeometry((labelButtonBottom->width()-30)/2,(labelButtonBottom->height()-14)/2,30,14);
+    labelButtonTopArrow->setGeometry(this->width()/2-25,(labelButtonTop->height()-14)/2,30,14);
     labelButtonBottomArrow->setGeometry(this->width()/2-25,(labelButtonTop->height()-14)/2,30,14);
 
     resizeEvent(nullptr);
