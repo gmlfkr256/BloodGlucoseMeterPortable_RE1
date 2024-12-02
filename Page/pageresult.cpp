8ad9f3808cb22d732578ae873b5c803c55412450
hrkim <rlagmlfkr@naver.com>
2024-12-02 15:47:30 +0900

    자동 커밋: 2024-12-02 15:47:30

--- Page/pageresult.cpp
+++ Page/pageresult.cpp
@@ -104,7 +104,7 @@ void PageResult::setColorValue(int nGlucoseValue)
     BloodSugarLevel bloodSugarLevel = instance.getBloodSugarLevel(nGlucoseValue);
     int nBloodSugarIndex = static_cast<int>(bloodSugarLevel);
 
-    strResult = "<p style='font-weight:bold; line-height: 0.5;'>"+strTextStatus+" </p>";
+    strResult = "<p style='font-weight:bold; line-height: 1.0;'>"+strTextStatus+" </p>";
 
     switch (instance.getDeviceLanguage())
     {
