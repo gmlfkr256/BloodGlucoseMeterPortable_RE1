311b2133e280ab9bdd9c66a5fd550f008a841fc7
hrkim <rlagmlfkr@naver.com>
2024-12-24 14:12:13 +0900

    자동 커밋: 2024-12-24 14:12:13

--- Page/pagedatetime.cpp
+++ Page/pagedatetime.cpp
@@ -77,7 +77,7 @@ void PageDateTime::setDateStatus(DateStatus dateStatus)
         if(com->dateStatus == dateStatus)
             com->isSelect = true;
 
-        if(com->dateStatus == DATE_DAY)
+        if(com->dateStatus == DATE_DAY || com->dateStatus == DATE_YEAR || com->dateStatus == DATE_MONTH)
         {
             QDate date(comDateYear->getDateValue(),comDateMonth->getDateValue(),1);
             com->nDayMax = date.daysInMonth();
