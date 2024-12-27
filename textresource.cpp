d743eb91a1597b951fd7e71874a788f09b0cbb69
hrkim <rlagmlfkr@naver.com>
2024-12-27 16:13:34 +0900

    자동 커밋: 2024-12-27 16:13:34

--- textresource.cpp
+++ textresource.cpp
@@ -118,7 +118,7 @@ void TextResource::init()
                                                     "사용자 정보를 삭제하시겠습니까?", //PASSWORD_STR_DELETE,
                                                     "사용자 정보를 삭제할 경우\n 비밀번호와 설정이 초기화 됩니다", //PASSWORD_STR_DELETE_CONFIRM,
                                                     "사용자 정보가 삭제되었습니다\n설정 초기화를 위해 재부팅이 진행됩니다", //PASSWORD_STR_DELETE_SUCCESS,
-                                                    "로그아웃을 진행합니까?\n로그아웃시 재로그인해주시기 바랍니다", //PASSWORD_STR_LOGOUT,
+                                                    "로그아웃을 진행합니까?\n로그아웃시 다시 로그인해주시기 바랍니다", //PASSWORD_STR_LOGOUT,
                                                     "PASSWORD_STR_MAX" //PASSWORD_STR_MAX
                                                 });
 
