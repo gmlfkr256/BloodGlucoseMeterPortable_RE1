ffd8a08d57940d690f9796aec436aa7f2bc4f05b
hrkim <rlagmlfkr@naver.com>
2025-01-07 17:54:12 +0900

    자동 커밋: 2025-01-07 17:54:12

--- Loadfont.cpp
+++ Loadfont.cpp
@@ -8,9 +8,8 @@ LoadFont::LoadFont()
 void LoadFont::Load()
 {
     int fontId = QFontDatabase::addApplicationFont(":/Font/Suit/SUIT-Regular.ttf");
-    //instance.fontSuit = QFontDatabase::applicationFontFamilies(fontId).at(0);
-    qDebug()<<"font: "<<QFontDatabase::applicationFontFamilies(fontId);
-    /*
+    instance.fontSuit = QFontDatabase::applicationFontFamilies(fontId).at(0);
+
     fontId = QFontDatabase::addApplicationFont(":/Font/JP/NotoSansJP-Regular.ttf");
     instance.fontJP = QFontDatabase::applicationFontFamilies(fontId).at(0);
 
@@ -19,5 +18,4 @@ void LoadFont::Load()
 
     fontId = QFontDatabase::addApplicationFont(":/Font/TC/NotoSansTC-Regular.ttf");
     instance.fontTC = QFontDatabase::applicationFontFamilies(fontId).at(0);
-    */
 }
