bf9b05db9ffe4ba5b5295948e241cab2d74e2311
hrkim <rlagmlfkr@naver.com>
2025-01-07 15:02:55 +0900

    자동 커밋: 2025-01-07 15:02:55

--- Page/pagetrans.cpp
+++ Page/pagetrans.cpp
@@ -43,7 +43,7 @@ void PageTrans::init()
 
 void PageTrans::update()
 {
-    labelButton[0]->setStyleSheet("border: solid 3px #eeeeee; border-radius: 15px; padding-left: 30px;");
+    labelButton[0]->setStyleSheet("background-color: white; border: solid 3px #eeeeee; border-radius: 15px; padding-left: 30px;");
     labelButton[1]->setStyleSheet("border: solid 3px #077bdd; border-radius: 15px; padding-left: 30px;");
     labelButton[2]->setStyleSheet("border: solid 3px #eeeeee; border-radius: 15px; padding-left: 30px;");
 
