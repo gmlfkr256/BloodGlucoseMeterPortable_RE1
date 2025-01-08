a0b36c5f1fb40e1b6f3d4839b047c4facf015db5
hrkim <rlagmlfkr@naver.com>
2025-01-08 14:42:10 +0900

    자동 커밋: 2025-01-08 14:42:10

--- Component/componentbluetooth.cpp
+++ Component/componentbluetooth.cpp
@@ -26,7 +26,7 @@ void ComponentBluetooth::update()
 #else
     resultBleAct = GAPI_SUCCESS;
     resultBleStat = GAPI_SUCCESS;
-    bleAct.act = GAPI_BLE_ACT_DISABLE;
+    //bleAct.act = GAPI_BLE_ACT_DISABLE;
 #endif
 
     if(resultBleAct !=GAPI_FAIL)
