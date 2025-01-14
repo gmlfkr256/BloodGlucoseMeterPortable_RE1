15fea07d7beef129624d0d50e75b1961a7909b24
hrkim <rlagmlfkr@naver.com>
2025-01-14 16:28:49 +0900

    자동 커밋: 2025-01-14 16:28:49

--- Loadfont.cpp
+++ Loadfont.cpp
@@ -21,43 +21,42 @@ void LoadFont::Load()
     instance.fontTC = QFontDatabase::applicationFontFamilies(fontId).at(0);
     */
     // 실행 파일 경로 얻기
-        QString executablePath = QCoreApplication::applicationDirPath();
-        QString fontPath = QDir(executablePath).filePath("font"); // "font" 디렉토리 경로
+    QString executablePath = QCoreApplication::applicationDirPath();
+    QString fontPath = QDir(executablePath).filePath("font"); // "font" 디렉토리 경로
 
-        // SUIT-Regular.ttf 로드
-        QString suitFont = QDir(fontPath).filePath("Suit/SUIT-Regular.ttf");
-        int fontId = QFontDatabase::addApplicationFont(suitFont);
-        if (fontId != -1) {
-            instance.fontSuit = QFontDatabase::applicationFontFamilies(fontId).at(0);
-        } else {
-            qWarning() << "Failed to load font:" << suitFont;
-        }
+    // SUIT-Regular.ttf 로드
+    QString suitFont = QDir(fontPath).filePath("Suit/SUIT-Regular.ttf");
+    int fontId = QFontDatabase::addApplicationFont(suitFont);
+    if (fontId != -1) {
+        instance.fontSuit = QFontDatabase::applicationFontFamilies(fontId).at(0);
+    } else {
+        qWarning() << "Failed to load font:" << suitFont;
+    }
 
-        // NotoSansJP-Regular.ttf 로드
-        QString jpFont = QDir(fontPath).filePath("JP/NotoSansJP-Regular.ttf");
-        fontId = QFontDatabase::addApplicationFont(jpFont);
-        if (fontId != -1) {
-            instance.fontJP = QFontDatabase::applicationFontFamilies(fontId).at(0);
-        } else {
-            qWarning() << "Failed to load font:" << jpFont;
-        }
+    // NotoSansJP-Regular.ttf 로드
+    QString jpFont = QDir(fontPath).filePath("JP/NotoSansJP-Regular.ttf");
+    fontId = QFontDatabase::addApplicationFont(jpFont);
+    if (fontId != -1) {
+        instance.fontJP = QFontDatabase::applicationFontFamilies(fontId).at(0);
+    } else {
+        qWarning() << "Failed to load font:" << jpFont;
+    }
 
-        // NotoSansSC-Regular.ttf 로드
-        QString scFont = QDir(fontPath).filePath("SC/NotoSansSC-Regular.ttf");
-        fontId = QFontDatabase::addApplicationFont(scFont);
-        if (fontId != -1) {
-            instance.fontSC = QFontDatabase::applicationFontFamilies(fontId).at(0);
-        } else {
-            qWarning() << "Failed to load font:" << scFont;
-        }
+    // NotoSansSC-Regular.ttf 로드
+    QString scFont = QDir(fontPath).filePath("SC/NotoSansSC-Regular.ttf");
+    fontId = QFontDatabase::addApplicationFont(scFont);
+    if (fontId != -1) {
+        instance.fontSC = QFontDatabase::applicationFontFamilies(fontId).at(0);
+    } else {
+        qWarning() << "Failed to load font:" << scFont;
+    }
 
-        // NotoSansTC-Regular.ttf 로드
-        QString tcFont = QDir(fontPath).filePath("TC/NotoSansTC-Regular.ttf");
-        fontId = QFontDatabase::addApplicationFont(tcFont);
-        if (fontId != -1) {
-            instance.fontTC = QFontDatabase::applicationFontFamilies(fontId).at(0);
-        } else {
-            qWarning() << "Failed to load font:" << tcFont;
-        }
-
+    // NotoSansTC-Regular.ttf 로드
+    QString tcFont = QDir(fontPath).filePath("TC/NotoSansTC-Regular.ttf");
+    fontId = QFontDatabase::addApplicationFont(tcFont);
+    if (fontId != -1) {
+        instance.fontTC = QFontDatabase::applicationFontFamilies(fontId).at(0);
+    } else {
+        qWarning() << "Failed to load font:" << tcFont;
+    }
 }
