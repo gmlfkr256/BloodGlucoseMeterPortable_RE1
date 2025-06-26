c17ba7ead2f0a0285376eafb042fe8ae5070eae6
hrkim <rlagmlfkr@naver.com>
2025-06-26 14:53:57 +0900

    자동 커밋: 2025-06-26 14:53:57

--- Page/pagegarph.cpp
+++ Page/pagegarph.cpp
@@ -314,6 +314,7 @@ void PageGarph::mousePressEvent(QMouseEvent *ev)
 
 void PageGarph::pageShow()
 {
+    instance.isBatCharging = true;
     if(instance.isBatCharging == true)
     {
         pageHide();
